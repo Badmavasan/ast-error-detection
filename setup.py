04f55736c4d2e1dfd5a24737b660d04b9238cfbc
Badmavasan <sunshinevasan@gmail.com>
2025-03-18 02:43:39 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="asterrdetection",
-    version="0.1",
+    version="0.2",
     description="A package for finding the list of errors in a code compared to the expected code",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -10,7 +10,10 @@ setup(
     author_email="badmavasan.kirouchenassamy@lip6.fr",
     url="https://github.com/Badmavasan/ast-error-detection",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "graphviz",
+        "numpy"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3",
